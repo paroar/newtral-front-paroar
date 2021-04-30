3d6478c9a7ac3ebd94173c2c609c606f921cf06e
paroar <paroar@pop-os.localdomain>
2021-04-30 23:08:23 +0200

    accordionContainer

---
+++ src/containers/accordionContainer.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-underscore-dangle */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Accordion } from '../components';
+
+import { filter, dictionary } from '../constants/csv_dictionary';
+
+const AccordionContainer = ({ data }) => {
+  const entries = Object.entries(data);
+
+  return (
+    <Accordion>
+      {entries.map((item) => (filter.includes(item[0])
+        ? null
+        : (
+          <Accordion.Item>
+            <Accordion.Header>{dictionary[item[0]]}</Accordion.Header>
+            <Accordion.Body>{(item[1] === null && 'Sin datos') || item[1]}</Accordion.Body>
+          </Accordion.Item>
+        )))}
+    </Accordion>
+  );
+};
+
+AccordionContainer.propTypes = {
+  data: PropTypes.objectOf.isRequired,
+};
+
+export default AccordionContainer;
